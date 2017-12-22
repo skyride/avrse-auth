9286204ba53dd1a0fadb044717e5c687c2a29de2
Adam Findlay <skylinerspeeder@gmail.com>
2017-12-22 19:36:12 +0000

    Added unauthenticated client factory

---
+++ esi/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+swagger_json_file = os.path.join(
+    os.path.dirname(__file__),
+    "swagger.json"
+)
---
+++ esi/swagger.py
@@ -0,0 +1,12 @@
+from bravado.client import SwaggerClient
+from bravado.swagger_model import load_file
+from bravado.requests_client import RequestsClient
+
+from esi import swagger_json_file
+
+
+def get_client(token=None):
+    if token == None:
+        return SwaggerClient.from_spec(
+            load_file(swagger_json_file)
+        )
