a691c5ca2ce2cbdbc8b8ae51c621f24512307e06
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-15 12:47:29 +0000

    Created timer model

--- timerboard/models.py
+++ timerboard/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from sde.models import Type, System
+
+
+class Timer(models.Model):
+    STAGE_CHOICES = (
+        ("AR", "Armor"),
+        ("ST", "Structure"),
+    )
+
+    structure = models.ForeignKey(Type, related_name="timers", on_delete=models.CASCADE)
+    system = models.ForeignKey(System, related_name="timers", on_delete=models.CASCADE)
+
+    date = models.DateTimeField(db_index=True)
+    stage = models.CharField(max_length=2, choices=STAGE_CHOICES, db_index=True)
+
+    created_by = models.ForeignKey(User, related_name="timers", on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
--- timerboard/views.py
+++ timerboard/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
+
 
 
 def index(request):
