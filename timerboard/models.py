e05ecdc7ed7bf8b04e8082cacba3ed7f4cedeceb
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-15 13:55:53 +0000

    Finished timer model

--- timerboard/models.py
+++ timerboard/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
 
 from sde.models import Type, System
@@ -13,11 +13,28 @@ class Timer(models.Model):
         ("ST", "Structure"),
     )
 
+    VISIBLE_TO_LEVEL_CHOICES = (
+        (0, "Non-Members"),
+        (1, "Blues"),
+        (2, "Members"),        
+    )
+
     structure = models.ForeignKey(Type, related_name="timers", on_delete=models.CASCADE)
     system = models.ForeignKey(System, related_name="timers", on_delete=models.CASCADE)
 
     date = models.DateTimeField(db_index=True)
     stage = models.CharField(max_length=2, choices=STAGE_CHOICES, db_index=True)
 
+    visible_to_level = models.IntegerField(choices=VISIBLE_TO_LEVEL_CHOICES, default=2)
+    visible_to_groups = models.ManyToManyField(Group, related_name="timers", blank=True)
+
     created_by = models.ForeignKey(User, related_name="timers", on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s %s-%s on %s" % (
+            self.id,
+            self.structure.name,
+            self.stage,
+            self.date
+        )
