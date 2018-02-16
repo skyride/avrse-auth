e59aeb833912d02298f0ebd3e3ee399e43da1247
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-16 10:53:01 +0000

    Improved index listing

--- timerboard/templatetags/timertags.py
+++ timerboard/templatetags/timertags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter(name="user_can_edit")
 def user_can_edit(timer, user):
     return timer.user_can_edit(user)
+
+@register.filter(name="side_class")
+def side_class(timer):
+    return "text-" + [
+        "info",
+        "danger",
+        'warning'
+    ][timer.side]
