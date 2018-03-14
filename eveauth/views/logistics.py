6fdc132cd9d979bc473a33131c367935efd6eb61
Adam Findlay <skylinerspeeder@gmail.com>
2018-03-14 20:06:09 +0000

    Added system options

--- eveauth/views/logistics.py
+++ eveauth/views/logistics.py
@@ -47,7 +47,7 @@ def structures_index(request):
 
 @login_required
 @user_passes_test(lambda x: x.groups.filter(name__in=["admin", "logistics"]).exists())
-def structures_list(request, corp_id, order_by):
+def structures_list_corp(request, corp_id, order_by):
     corp = Corporation.objects.get(id=corp_id)
 
     order_by = {
@@ -61,6 +61,7 @@ def structures_list(request, corp_id, order_by):
 
     context = {
         "corp": corp,
+        "structures_list": "structures_list_corp",
         "structures": corp.structures.annotate(
                 service_count=Count('services'),
                 timer=Coalesce('state_timer_end', 'fuel_expires')
@@ -79,4 +80,42 @@ def structures_list(request, corp_id, order_by):
             )
     }
 
-    return render(request, "eveauth/logistics/structures_list.html", context)
+    return render(request, "eveauth/logistics/structures_list_corp.html", context)
+
+
+@login_required
+@user_passes_test(lambda x: x.groups.filter(name__in=["admin", "logistics"]).exists())
+def structures_list_system(request, system_id, order_by):
+    system = System.objects.get(id=system_id)
+
+    order_by = {
+        "": "system__region__name",
+        "fuel": F("timer").asc(nulls_last=True),
+        "type": 'type__name',
+        "services": '-service_count',
+        "name": 'station__name',
+        "state": 'state'
+    }[order_by]
+
+    context = {
+        "system": system,
+        "structures_list": "structures_list_system",
+        "structures": system.structures.annotate(
+                service_count=Count('services'),
+                timer=Coalesce('state_timer_end', 'fuel_expires')
+            ).prefetch_related(
+                'system',
+                'system__region',
+                'type',
+                'station'
+            ).order_by(
+                order_by,
+                'system__region__name',
+                'system__name',
+                F("timer").asc(nulls_last=True),
+                '-type__mass',
+                'station__name'
+            )
+    }
+
+    return render(request, "eveauth/logistics/structures_list_system.html", context)
