e1c877b9f12c4a8425b4c4580b93061537db5d47
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-03-23 17:35:37 +0000

    Added access level field to group details

--- eveauth/models/groupdetails.py
+++ eveauth/models/groupdetails.py
@@ -5,10 +5,17 @@ from django.dispatch import receiver
 
 
 class GroupDetails(models.Model):
+    ACCESS_LEVEL_CHOICES = (
+        (0, 'Non-Members'),
+        (1, 'Blues'),
+        (2, "Members"),
+    )
+
     group = models.OneToOneField(Group, related_name="details", on_delete=models.CASCADE)
 
     is_open = models.BooleanField(default=False)
     can_apply = models.BooleanField(default=False)
+    access_level = models.IntegerField(default=2, choices=ACCESS_LEVEL_CHOICES)
 
     mumble = models.BooleanField(default=True)
     forum = models.BooleanField(default=False)
